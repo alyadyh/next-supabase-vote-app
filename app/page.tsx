037a4275585807a700905c16b9f1c5773d2df191
Alya Dhiya <alya.mardhiyyah@binus.ac.id>
2024-04-23 14:47:10 +0700

    Adding permit from permitio

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,34 @@
-"use client";
+import { permit } from "@/lib/permit";
 
-import { supabase } from "@/lib/supabase";
-import { useRouter } from "next/navigation";
+export const dynamic = 'force-dynamic';
 
-export default function Home() {
-  const router = useRouter();
+const getUsers = async () => {
+  const result = await permit.api.listUsers()
+  return result
+}
 
-  const logout = async () => {
-    await supabase
-      .auth
-      .signOut()
+export default async function Home() {
+  const users = await getUsers();
 
-    router.refresh();
-  }
-
-  return <div>Logged in
-    <button onClick={logout} className="px-2 py-1 bg-blue-500 text-white cursor-pointer">Logout</button>
+  return <div className="container grid gap-6 my-12 mx-auto max-w-[1000px]">
+    <header className="grid gap-2">
+      <h1>List of users</h1>
+      <p>Here is a list of users coming from Permit.</p>
+    </header>
+    <div>
+      <ul>
+        {users?.map((user: any, key: number) => <li className="flex gap-2" key={key}>
+          <span>{user.email}</span>
+          <div>
+            {user.roles?.map((obj: any, roleKey: number) =>
+              <span className="bg-purple-100 text-purple-600 px-2 py-1 rounded-full" key={roleKey}>{obj.role}</span>
+            )}
+          </div>
+        </li>)}
+      </ul>
+    </div>
+    <footer>
+      <button>+ Add a new user</button>
+    </footer>
   </div>
 }
