c66d8ef020bb29d598336a9613d48e2c76329ed4
Alya Dhiya <alya.mardhiyyah@binus.ac.id>
2024-04-23 14:55:24 +0700

    Adding vote up

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,44 @@
-import { permit } from "@/lib/permit";
+"use client";
 
-export const dynamic = 'force-dynamic';
+import ArticleItem from "@/components/ArticleItem";
+import { useArticles } from "@/hooks/useArticles";
+import { supabase } from "@/lib/supabase";
+import { useEffect } from "react";
 
-const getUsers = async () => {
-  const result = await permit.api.listUsers()
-  return result
-}
+export default function Home() {
+  const { articles, getArticles } = useArticles()
 
-export default async function Home() {
-  const users = await getUsers();
+  // const subscribedChannel = supabase
+  //   .channel('articles-follow-up')
+  //   .on('postgres_changes', {
+  //     event: '*',
+  //     schema: 'public',
+  //     table: 'articles'
+  //   }, (payload: any) => {
+  //     console.log(payload)
+  //   })
+  //   .subscribe();
 
-  return <div className="container grid gap-6 my-12 mx-auto max-w-[1000px]">
-    <header className="grid gap-2">
-      <h1>List of users</h1>
-      <p>Here is a list of users coming from Permit.</p>
-    </header>
-    <div>
-      <ul>
-        {users?.map((user: any, key: number) => <li className="flex gap-2" key={key}>
-          <span>{user.email}</span>
-          <div>
-            {user.roles?.map((obj: any, roleKey: number) =>
-              <span className="bg-purple-100 text-purple-600 px-2 py-1 rounded-full" key={roleKey}>{obj.role}</span>
-            )}
-          </div>
-        </li>)}
-      </ul>
+  const subscribedChannel = supabase
+    .channel('articles-follow-up')
+    .on('postgres_changes', {
+      event: '*',
+      schema: 'public',
+      table: 'votes'
+    }, async (payload: any) => {
+      await getArticles()
+    })
+    .subscribe();
+
+  useEffect(() => {
+    getArticles()
+  }, []);
+
+  return <div className="container mx-auto my-8">
+    <div className="grid gap-4">
+      {articles.map((article: any, key: number) => {
+        return <ArticleItem key={key} article={article} />
+      })}
     </div>
-    <footer>
-      <button>+ Add a new user</button>
-    </footer>
   </div>
 }
