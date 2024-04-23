25974c73f271c15cb3675e33de84f9b47b95b84c
Alya Dhiya <alya.mardhiyyah@binus.ac.id>
2024-04-23 14:37:04 +0700

    Using hooks

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,20 @@
+"use client";
+
+import { useArticles } from "@/hooks/useArticles";
+import { useEffect } from "react";
 
 export default function Home() {
-  return <div>You are logged in</div>
+  const { articles, getArticles } = useArticles()
+
+  useEffect(() => {
+    getArticles()
+  }, []);
+
+  return <div>
+    <ul>
+      {articles.map((article: any, key: number) => {
+        return <li key={key}>{article.title}</li>
+      })}
+    </ul>
+  </div>
 }
